656c30754f23643b7b9c895b1a7bb7b6ff76d833
galihagungs <galihagung223@gmail.com>
2025-04-14 01:20:19 +0700

    Add login page and utility styles
    - Created a new LoginPage widget in `lib/login.dart` with a placeholder for future implementation.
    - Introduced utility styles in `lib/utils/utils.dart` including color definitions and text styles using Google Fonts.

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,4 +1,9 @@
+import 'package:absenku/login.dart';
+import 'package:absenku/utils/utils.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:lottie/lottie.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -8,8 +13,151 @@ class OnboardingPage extends StatefulWidget {
 }
 
 class _OnboardingPageState extends State<OnboardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+  void _onIntroEnd(context) {
+    Navigator.of(
+      context,
+    ).pushReplacement(MaterialPageRoute(builder: (_) => const LoginPage()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // const bodyStyle = TextStyle(fontSize: 19.0);
+    // const pageDecoration = PageDecoration(
+    //   titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+    //   bodyTextStyle: bodyStyle,
+    //   bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+    //   pageColor: Colors.white,
+    //   imagePadding: EdgeInsets.zero,
+    // );
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: bgColor,
+      allowImplicitScrolling: true,
+      autoScrollDuration: 3000,
+
+      pages: [
+        PageViewModel(
+          title: "",
+          decoration: PageDecoration(bodyAlignment: Alignment.center),
+          bodyWidget: Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+              Stack(
+                children: [
+                  Image.asset("assets/images/onboarding1.png", width: 350),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.50,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Provides attendance management",
+                          style: kanit30bold,
+                        ),
+                        SizedBox(height: 25),
+                        Text(
+                          "Make it easy to manage your employee attendance data",
+                          style: kanit16normal,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        PageViewModel(
+          title: "",
+          decoration: PageDecoration(bodyAlignment: Alignment.center),
+          bodyWidget: Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+              Stack(
+                children: [
+                  Lottie.asset(
+                    'assets/images/onboarding2.json',
+                    width: 350,
+                    repeat: false,
+                    fit: BoxFit.fitWidth,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.42,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Track \n Attendance Location",
+                          style: kanit30bold,
+                        ),
+                        SizedBox(height: 25),
+                        Text(
+                          "Provide GPS solution to track employee attendance",
+                          style: kanit16normal,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        PageViewModel(
+          title: "",
+          decoration: PageDecoration(bodyAlignment: Alignment.center),
+          bodyWidget: Column(
+            children: [
+              // SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+              Image.asset('assets/images/logonobox.png', width: 300),
+              Text("This is the solution for you", style: kanit25bold),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  backgroundColor: WidgetStatePropertyAll(mainColor),
+                ),
+                child: Text("Let's get started", style: kanit20normalWhite),
+              ),
+            ],
+          ),
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      controlsPosition: Position.fromLTRB(
+        -100,
+        MediaQuery.of(context).size.height * 0.85,
+        200,
+        0,
+      ),
+      showBackButton: false,
+      showNextButton: false,
+      showDoneButton: false,
+      back: const Icon(Icons.arrow_back),
+      skip: const Text('Skip', style: TextStyle(fontWeight: FontWeight.w600)),
+      next: const Icon(Icons.arrow_forward),
+      done: Text('Done', style: poppins16italic),
+      curve: Curves.fastLinearToSlowEaseIn,
+
+      dotsDecorator: DotsDecorator(
+        size: const Size.square(10.0),
+        activeSize: const Size(20.0, 10.0),
+        activeColor: mainColor,
+        color: Colors.black26,
+
+        spacing: const EdgeInsets.symmetric(horizontal: 0.5),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/utils.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//Colors
+Color mainColor = const Color(0xff006dbf);
+Color bgColor = const Color(0xffe5e9fe);
+
+//TextStyle
+TextStyle poppins16italic = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+  fontStyle: FontStyle.italic,
+);
+TextStyle kanit30bold = GoogleFonts.kanit(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+);
+TextStyle kanit25bold = GoogleFonts.kanit(
+  fontSize: 25,
+  fontWeight: FontWeight.w700,
+  color: Color(0xff006dbf),
+);
+TextStyle kanit16normal = GoogleFonts.kanit(
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+);
+TextStyle kanit20normalWhite = GoogleFonts.kanit(
+  fontSize: 20,
+  fontWeight: FontWeight.w300,
+  color: Colors.white,
+);
